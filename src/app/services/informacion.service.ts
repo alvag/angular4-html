d9055913739e1a263868b52660f3ac04e6e4e1ac
Max Alva <alva85@gmail.com>
2017-09-11 23:48:30 -0500

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,13 +5,27 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info: any = {};
+  equipo: any[] = [];
 
   constructor(public http: Http) {
+    this.cargaInfo();
+    this.carga_sobre_nosotros();
+  }
+
+  public cargaInfo() {
     this.http.get('assets/data/info.pagina.json')
       .subscribe(data => {
-        console.log(data.json());
+        // console.log(data.json());
         this.info = data.json();
       });
-   }
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get('https://angular-4-html.firebaseio.com/equipo.json')
+      .subscribe(data => {
+        console.log(data.json());
+        this.equipo = data.json();
+      });
+  }
 
 }
